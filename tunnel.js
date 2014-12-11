64de1384a3dfd2dd002651f50d8989e28602ee25
Nathan Vander Wilt <nate@calftrail.com>
2014-12-10 21:15:01 -0800

    all (current…) tests passing!

--- tunnel.js
+++ tunnel.js
@@ -14,21 +14,27 @@ function Tunnel(opts) {
         encoding: null
     });
     
-    this._messenger = new Messenger();
-    this._messenger.on('json:0', function (d) {
+    var self = this;
+    self._messenger = new Messenger();
+    self._messenger.on('json:0', function (d) {
         if (d.type === 'stream') {
             var stream = new Substream(this, d.sock);
-            this.emit('stream', stream, d.name);
+            self.emit('stream', stream, d.name);
         } else {
             console.warn("Unknown control message:", d);
         }
     });
+    self._messenger.on('data', function (d) {
+        var more = self.push(d);
+        if (!more) self._messenger.pause();
+    });
+    
 }
 util.inherits(Tunnel, stream.Duplex);
 
 // TODO: inherit from Messenger instead?
 Tunnel.prototype._read = function (size) {
-    return this._messenger.read(size);
+    this._messenger.resume();
 };
 Tunnel.prototype._write = function (buf, enc, cb) {
     return this._messenger.write(buf, enc, cb);
